586716370e2c9ebd5786d944f93e2a14a80a0b48
Your Name <shister82@gmail.com>
2017-12-26 01:35:41 -0500

    added playlist ability

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var fs = require('fs');
     app = express();
 var Discord = require('discord.io');
+var yt_playlist = require('./ytplaylist');
 function Queue()
 {
  this.stac=new Array();
@@ -54,120 +55,43 @@ function Queue()
         var botMention = "<@" + bot.id + ">";
         if(userID != bot.id && message.includes(botMention)){
           if(message.includes("siege")){
-            var siege_username = "";
-            siege_username = getSiegeUsername(message);
-            bot.simulateTyping(channelID);
-            //Operators
-            if(message.includes('-o')){
-              siege_username = siege_username.substring(0,siege_username.indexOf('-o'));
-              var operator = message.substring(message.indexOf('-o') + 2);
-              operator = operator.trim();
-              var url = 'https://api.r6stats.com/api/v1/players/' + siege_username + '/operators?platform=uplay'
-              var msg = "";
-              request(url, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                  result = JSON.parse(body);
-                  for(var obj in result.operator_records){
-                    if(result.operator_records[obj].operator.name.toLowerCase() == operator.toLowerCase()){
-                      msg = "Operator: " + result.operator_records[obj].operator.name + "\n" + "Rounds Played: " + result.operator_records[obj].stats.played + "\n"
-                      + "Wins: " + result.operator_records[obj].stats.wins + "\n" + "Loses: " + result.operator_records[obj].stats.losses + "\n" + "Win %: " + result.operator_records[obj].stats.wins/ (result.operator_records[obj].stats.wins + result.operator_records[obj].stats.losses)  + "\nKills: " + result.operator_records[obj].stats.kills
-                      + "\n" + "Deaths: " + result.operator_records[obj].stats.deaths + "\n";
-                    }
-                  }
-                  bot.sendMessage({
-                      to: channelID,
-                      message: msg
-                  });
-                }
-                else{
-                  bot.sendMessage({
-                    to: channelID,
-                    message: "Error:\n" + error
-                  })
-                }
-              });
-            }
-            else{ //Player Data
-              var url = 'https://api.r6stats.com/api/v1/players/' + siege_username + '?platform=uplay'
-              request(url, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                  result = JSON.parse(body);
-                  bot.sendMessage({
-                      to: channelID,
-                      message: "Username: " + result.player.username +"\nLevel: "+ result.player.stats.progression.level + "\nLast Updated: " + result.player.updated_at + "\n\n"
-                      + "Ranked Stats: \n" + "Wins: " + result.player.stats.ranked.wins + "\n" + "Losses: " + result.player.stats.ranked.losses + "\n"
-                      + "Win Rate: " + result.player.stats.ranked.wins / (result.player.stats.ranked.wins + result.player.stats.ranked.losses) + "\n"
-                      + "Kills: " + result.player.stats.ranked.kills + "\n" + "Deaths: " + result.player.stats.ranked.deaths + "\n" + "K/D: " + result.player.stats.ranked.kd + "\n"
-                      + "\n" + "Casual Stats:\n" + "Wins: " + result.player.stats.casual.wins + "\nLosses: " + result.player.stats.casual.losses + "\n" + "Win Rate: " + result.player.stats.casual.wins/(result.player.stats.casual.wins + result.player.stats.casual.losses)+
-                      "\n" + "Kills: " + result.player.stats.casual.kills + "\n" + "Deaths: " + result.player.stats.casual.deaths + "\n" + "K/D: " + result.player.stats.casual.kd + "\n\n" + "Meme Stats: \n" + "Revives: " + result.player.stats.overall.revives + "\nSuicides: " +
-                      result.player.stats.overall.suicides + "\nReinforcements Deployed: " + result.player.stats.overall.reinforcements_deployed + "\nBarricades Built: " + result.player.stats.overall.barricades_built + "\nSteps Moved: " + result.player.stats.overall.steps_moved + "\nHit Percentage: "
-                      + (result.player.stats.overall.bullets_hit/result.player.stats.overall.bullets_fired) + "\nHeadshots: " + result.player.stats.overall.headshots + "\nMelee Kills: " + result.player.stats.overall.melee_kills + "\nPenetration Kills: " + result.player.stats.overall.penetration_kills + "\n"
-                      + "Assists: " + result.player.stats.overall.assists + "\n"
-                      + url + "\n"
-                  });
-                }
-                else{
-                  bot.sendMessage({
-                    to: channelID,
-                    message: "Error:\n" + error
-                  })
-                }
-              });
-            }
+            handleSiege(message,channelID)
           }
           else if(message.includes("play")){
+            if(message.includes('https://www.youtube.com/playlist?list=')){
+              getUserVoiceChannelID(userID);
+              var playlist1 = new yt_playlist(settings.youtube);
+              var listId = message.substring(message.indexOf('?list=') + 6)//'PL6gx4Cwl9DGBMdkKFn3HasZnnAqVjzHn_';
+              var volume = DEFAULT_AUDIO_VOLUME;
+              playlist1.parsePlaylist(listId).then(function(res) {
+                  for(var i in res.items){
+                    if(res.items[i].contentDetails != null){
+                      //console.log(res.items[i].contentDetails);
+                      if(res.items[i].contentDetails.videoId != null){
+                        var ub = 'https://www.youtube.com/watch?v=' + res.items[i].contentDetails.videoId;
+                        var item = {url: ub, volume: volume, channel: channelID, title: ''};
+                        playlist.push(item); //Todo: make this a class/function
+                      }
+                    }
+                  }
+                  if(!isPlaying){
+                    setTimeout(function(){
+                      playNext(playlist);
+                    }, 3000);
+                  }
+              }).catch(function(error) {
+                  console.log(error);
+              });
 
-            yt_url = message.substring(message.indexOf('play') + 4).trim();
-
-            for(var i in bot.channels){
-              var channel = bot.channels[i];
-              for(var u in channel.members){
-                if(channel.members[u].user_id == userID){
-                  voiceChannelID = channel.members[u].channel_id;
-                  break;
-                }
-              }
             }
-            if(message.includes('volume')){
-              try{
-                audioVolume = parseFloat(message.substring(message.indexOf('volume') + 6));
-                yt_url = yt_url.substring(0,yt_url.indexOf('volume') - 1);
-                console.log('av',audioVolume)
-                console.log('url',yt_url);
-              }catch(err){
-                audioVolume = DEFAULT_AUDIO_VOLUME;
-                bot.sendMessage({
-                  to: channelID,
-                  message: "Error: Invalid volume(0 - 1.0)"
-                })
-              }
-            } else{
-              audioVolume = DEFAULT_AUDIO_VOLUME;
+            else if(message.includes('https://www.youtube.com/watch?v=')){
+              playYouTubeURL(message,channelID,userID);
             }
-            var item = {url: yt_url, volume: audioVolume, channel: channelID, title: ''};
-            ytdl.getInfo(yt_url, {quality:"highest"}, function(err,info){
-              if(info != null){
-                item.title = info.title;
-              }
-              playlist.push(item); //Todo: make this a class/function
-              if(isPlaying){
-                bot.sendMessage({
-                  to: channelID,
-                  message: "Added song to playlist"
-                })
-              }
-              if(!isPlaying){
-                setTimeout(function(){
-                  playNext();
-                }, 3000);
-              }
-            })
 
           }
           else if(message.includes("skip")){
             bot.leaveVoiceChannel(voiceChannelID);
             bot.joinVoiceChannel(voiceChannelID, function(error){});
-            //playNext(); //trigger the scoket error
           }
           else if(message.includes("stop")){
               bot.leaveVoiceChannel(voiceChannelID);
@@ -204,12 +128,90 @@ function Queue()
       }
       //process.exit(1);             // exit with error
     });
+function getUserVoiceChannelID(userID){
+  for(var i in bot.channels){
+    var channel = bot.channels[i];
+    for(var u in channel.members){
+      if(channel.members[u].user_id == userID){
+        voiceChannelID = channel.members[u].channel_id;
+        break;
+      }
+    }
+  }
+  return voiceChannelID;
+}
 function getSiegeUsername(message){
   var siege_username = "";
   var index = message.indexOf('siege') + 5;
   var temp = message.substring(index);
   siege_username = temp.trim();
   return siege_username;
+}
+function handleSiege(message,channelID){
+  var siege_username = "";
+  siege_username = getSiegeUsername(message);
+  bot.simulateTyping(channelID);
+  //Operators
+  if(message.includes('-o')){
+    siege_username = siege_username.substring(0,siege_username.indexOf('-o'));
+    var operator = message.substring(message.indexOf('-o') + 2);
+    operator = operator.trim();
+    var url = 'https://api.r6stats.com/api/v1/players/' + siege_username + '/operators?platform=uplay'
+    var msg = "";
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        result = JSON.parse(body);
+        for(var obj in result.operator_records){
+          var api_operator = result.operator_records[obj].operator.name.toLowerCase();
+          if(api_operator == 'jäger'){
+            api_operator = 'jaeger';
+          }
+          if(api_operator == operator.toLowerCase()){
+            msg = "Operator: " + result.operator_records[obj].operator.name + "\n" + "Rounds Played: " + result.operator_records[obj].stats.played + "\n"
+            + "Wins: " + result.operator_records[obj].stats.wins + "\n" + "Loses: " + result.operator_records[obj].stats.losses + "\n" + "Win %: " + result.operator_records[obj].stats.wins/ (result.operator_records[obj].stats.wins + result.operator_records[obj].stats.losses)  + "\nKills: " + result.operator_records[obj].stats.kills
+            + "\n" + "Deaths: " + result.operator_records[obj].stats.deaths + "\n";
+          }
+        }
+        bot.sendMessage({
+            to: channelID,
+            message: msg
+        });
+      }
+      else{
+        bot.sendMessage({
+          to: channelID,
+          message: "Syntax Error"
+        })
+      }
+    });
+  }
+  else{ //Player Data
+    var url = 'https://api.r6stats.com/api/v1/players/' + siege_username + '?platform=uplay'
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        result = JSON.parse(body);
+        bot.sendMessage({
+            to: channelID,
+            message: "Username: " + result.player.username +"\nLevel: "+ result.player.stats.progression.level + "\nLast Updated: " + result.player.updated_at + "\n\n"
+            + "Ranked Stats: \n" + "Wins: " + result.player.stats.ranked.wins + "\n" + "Losses: " + result.player.stats.ranked.losses + "\n"
+            + "Win Rate: " + result.player.stats.ranked.wins / (result.player.stats.ranked.wins + result.player.stats.ranked.losses) + "\n"
+            + "Kills: " + result.player.stats.ranked.kills + "\n" + "Deaths: " + result.player.stats.ranked.deaths + "\n" + "K/D: " + result.player.stats.ranked.kd + "\n"
+            + "\n" + "Casual Stats:\n" + "Wins: " + result.player.stats.casual.wins + "\nLosses: " + result.player.stats.casual.losses + "\n" + "Win Rate: " + result.player.stats.casual.wins/(result.player.stats.casual.wins + result.player.stats.casual.losses)+
+            "\n" + "Kills: " + result.player.stats.casual.kills + "\n" + "Deaths: " + result.player.stats.casual.deaths + "\n" + "K/D: " + result.player.stats.casual.kd + "\n\n" + "Meme Stats: \n" + "Revives: " + result.player.stats.overall.revives + "\nSuicides: " +
+            result.player.stats.overall.suicides + "\nReinforcements Deployed: " + result.player.stats.overall.reinforcements_deployed + "\nBarricades Built: " + result.player.stats.overall.barricades_built + "\nSteps Moved: " + result.player.stats.overall.steps_moved + "\nHit Percentage: "
+            + (result.player.stats.overall.bullets_hit/result.player.stats.overall.bullets_fired) + "\nHeadshots: " + result.player.stats.overall.headshots + "\nMelee Kills: " + result.player.stats.overall.melee_kills + "\nPenetration Kills: " + result.player.stats.overall.penetration_kills + "\n"
+            + "Assists: " + result.player.stats.overall.assists + "\n"
+            + url + "\n"
+        });
+      }
+      else{
+        bot.sendMessage({
+          to: channelID,
+          message: "Error:\n" + error
+        })
+      }
+    });
+  }
 }
 function playVideo(vci, u, aV) {
   console.log(u);
@@ -236,21 +238,21 @@ function playVideo(vci, u, aV) {
           })
           isPlaying = false;
           setTimeout(function(){
-            playNext();
+            playNext(playlist);
           }, 3000);
         });
       });
 
   });
 }
-function playNext(){
+function playNext(playlist){
   if(playlist.length() > 0){
     var current = playlist.pop();
     console.log(current);
     if(voiceChannelID != null){
 
       playVideo(voiceChannelID,current.url,current.volume);
-      if(current.title != null){
+      if(current.title != ''){
         bot.sendMessage({
           to: current.channel,
           message: "Now Playing: " + current.title
@@ -260,6 +262,27 @@ function playNext(){
             game: {
             name: current.title
             }
+        })
+      } else {
+        var t= '';
+        ytdl.getInfo(current.url, {quality:"highest"}, function(err,info){
+          if(info != null){
+            t = info.title;
+          }
+          setTimeout(function(){
+            bot.sendMessage({
+              to: current.channel,
+              message: "Now Playing: " + t
+            })
+            bot.setPresence({
+                idle_since: null,
+                game: {
+                name: t
+                }
+            })
+          }, 3000);
+
+
         })
       }
 
@@ -273,6 +296,47 @@ function playNext(){
     voiceChannelID = null;
   }
 }
+function playYouTubeURL(message, channelID, userID){
+  yt_url = message.substring(message.indexOf('play') + 4).trim();
+
+  getUserVoiceChannelID(userID);
+  if(message.includes('volume')){
+    try{
+      audioVolume = parseFloat(message.substring(message.indexOf('volume') + 6));
+      yt_url = yt_url.substring(0,yt_url.indexOf('volume') - 1);
+      console.log('av',audioVolume)
+      console.log('url',yt_url);
+    }catch(err){
+      audioVolume = DEFAULT_AUDIO_VOLUME;
+      bot.sendMessage({
+        to: channelID,
+        message: "Error: Invalid volume(0 - 1.0)"
+      })
+    }
+  } else{
+    audioVolume = DEFAULT_AUDIO_VOLUME;
+  }
+  var item = {url: yt_url, volume: audioVolume, channel: channelID, title: ''};
+  ytdl.getInfo(yt_url, {quality:"highest"}, function(err,info){
+    if(info != null){
+      item.title = info.title;
+    }
+    playlist.push(item); //Todo: make this a class/function
+    if(isPlaying){
+      bot.sendMessage({
+        to: channelID,
+        message: "Added song to playlist"
+      })
+    }
+    if(!isPlaying){
+      setTimeout(function(){
+        playNext(playlist);
+      }, 3000);
+    }
+  })
+
+}
+
 var server = require('http').createServer(app);
 server.listen(9000);
 
