729507a60df9138443439c99e8cc65fe1375b524
Allen Shi <shister82@gmail.com>
2020-01-15 23:28:30 -0500

    updated settings

--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 
 module.exports = {
-  token: "MzkwOTg1ODY1MDAzOTI1NTA1.DRx76A.Nns6vkmQWoXMisA5p47Jbd7xJTE",
-  youtube: "AIzaSyAtPmuZRPRBozANNMb6ux_L3TTUhJW5XZk",
+  token: "",
+  youtube: "",
+  openai: ""
 }
